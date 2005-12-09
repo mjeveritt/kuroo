df6d29cbc0948655312fa9d4c889079031c1d289
karye <karye@1fa0634e-7644-4f51-99e6-489d0a0039cb>
2005-12-09 18:29:02 +0000

    z

---
+++ kuroo/branches/0.80.0/src/queue/emergeinspector.cpp
@@ -0,0 +1,211 @@
+
+#include "emergeinspector.h"
+#include "common.h"
+
+#include <qcheckbox.h>
+#include <qradiobutton.h>
+
+#include <klineedit.h>
+
+EmergeInspector::EmergeInspector( QWidget *parent, const char *name )
+	: KDialogBase( KDialogBase::Swallow, i18n( "Emerge Options" ), KDialogBase::User1 | KDialogBase::Ok | KDialogBase::Cancel, KDialogBase::Ok, parent, i18n( "Save" ), true )
+{
+	setButtonText( KDialogBase::User1, i18n( "Clear" ) );
+	dialog = new EmergeOptionsBase( this );
+	setMainWidget( dialog );
+}
+
+EmergeInspector::~EmergeInspector()
+{
+}
+
+/**
+ * Open dialog for advanced emerge options.
+ */
+void EmergeInspector::edit()
+{
+	show();
+
+	// Get options
+	QString options( dialog->emergeOptionsText->text() );
+	
+	if ( !options.isEmpty() ) {
+		const QStringList optionsList = QStringList::split( " ", options );
+		foreach ( optionsList ) {
+			options = ( *it ).stripWhiteSpace();
+			
+			if ( options == "--buildpkg" )
+				dialog->radioBuildpkg->toggle();
+			
+			if ( options == "--buildpkgonly" )
+				dialog->radioBuildpkgonly->toggle();
+			
+			if ( options == "--usepkg" )
+				dialog->radioUsepkg->toggle();
+			
+			if ( options == "--usepkgonly" )
+				dialog->radioUsepkgonly->toggle();
+			
+			if ( options == "--nodeps" )
+				dialog->radioNodeps->toggle();
+			
+			if ( options == "--onlydeps" )
+				dialog->radioOnlydeps->toggle();
+			
+			if ( options == "--verbose" )
+				dialog->radioVerbose->toggle();
+			
+			if ( options == "--quiet" )
+				dialog->radioQuiet->toggle();
+			
+			if ( options == "--pretend" )
+				dialog->checkPretend->toggle();
+			
+			if ( options == "--deep" )
+				dialog->checkDeep->toggle();
+			
+			if ( options == "--update" )
+				dialog->checkUpdate->toggle();
+			
+			if ( options == "--upgradeonly" )
+				dialog->checkUpgradeonly->toggle();
+			
+			if ( options == "--fetchonly" )
+				dialog->checkFetchonly->toggle();
+			
+			if ( options == "--emptytree" )
+				dialog->checkEmptytree->toggle();
+			
+			if ( options == "--debug" )
+				dialog->checkDebug->toggle();
+			
+			if ( options == "--noconfmem" )
+				dialog->checkNoconfmem->toggle();
+			
+			if ( options == "--oneshot" )
+				dialog->checkOneshot->toggle();
+			
+			if ( options == "--noreplace" )
+				dialog->checkNoreplace->toggle();
+			
+			if ( options == "--newuse" )
+				dialog->checkNewUse->toggle();
+		}
+	}
+}
+
+/**
+ * Write options back to lineedit
+ */
+void EmergeInspector::slotOk()
+{
+	QStringList optionsList;
+	
+	if ( dialog->radioBuildpkg->isOn() ) 
+		optionsList += "--buildpkg";
+	else
+		if ( dialog->radioBuildpkgonly->isOn() )
+			optionsList += "--buildpkgonly";
+	
+	if ( dialog->radioUsepkg->isOn() )
+		optionsList += "--usepkg";
+	else
+		if ( dialog->radioUsepkgonly->isOn() )
+			optionsList += "--usepkgonly";
+	
+	if ( dialog->radioNodeps->isOn() )
+		optionsList += "--nodeps";
+	else
+		if ( dialog->radioOnlydeps->isOn() )
+			optionsList += "--onlydeps";
+	
+	if ( dialog->radioVerbose->isOn() )
+		optionsList += "--verbose";
+	else
+		if ( dialog->radioQuiet->isOn() )
+			optionsList += "--quiet";
+	
+	if ( dialog->checkPretend->isChecked() )
+		optionsList += "--pretend";
+	
+	if ( dialog->checkDeep->isChecked() )
+		optionsList += "--deep";
+	
+	if ( dialog->checkUpdate->isChecked() )
+		optionsList += "--update";
+	
+	if ( dialog->checkUpgradeonly->isChecked() )
+		optionsList += "--upgradeonly";
+	
+	if ( dialog->checkFetchonly->isChecked() )
+		optionsList += "--fetchonly";
+	
+	if ( dialog->checkEmptytree->isChecked() )
+		optionsList += "--emptytree";
+	
+	if ( dialog->checkDebug->isChecked() ) 
+		optionsList += "--debug";
+	
+	if ( dialog->checkNoconfmem->isChecked() )
+		optionsList += "--noconfmem";
+	
+	if ( dialog->checkOneshot->isChecked() )
+		optionsList += "--oneshot";
+	
+	if ( dialog->checkNoreplace->isChecked() )
+		optionsList += "--noreplace";
+	
+	if ( dialog->checkNewUse->isChecked() )
+		optionsList += "--newuse";
+	
+	dialog->emergeOptionsText->setText( optionsList.join( " " ) );
+	close();
+}
+
+void EmergeInspector::slotUser1()
+{
+	if ( dialog->radioBuildpkg->isOn() )
+		dialog->radioBuildpkg->toggle();
+	
+	if ( dialog->radioBuildpkgonly->isOn() ) 
+		dialog->radioBuildpkgonly->toggle();
+	
+	if ( dialog->radioUsepkg->isOn() ) 
+		dialog->radioUsepkg->toggle();
+	
+	if ( dialog->radioUsepkgonly->isOn() ) 
+		dialog->radioUsepkgonly->toggle();
+	
+	if ( dialog->radioNodeps->isOn() ) 
+		dialog->radioNodeps->toggle();
+	
+	if ( dialog->radioOnlydeps->isOn() ) 
+		dialog->radioOnlydeps->toggle();
+	
+	if ( dialog->radioVerbose->isOn() ) 
+		dialog->radioVerbose->toggle();
+	
+	if ( dialog->radioQuiet->isOn() ) 
+		dialog->radioQuiet->toggle();
+	
+	dialog->checkPretend->setChecked( false );
+	dialog->checkDeep->setChecked( false );
+	dialog->checkUpdate->setChecked( false );
+	dialog->checkUpgradeonly->setChecked( false );
+	dialog->checkFetchonly->setChecked( false );
+	dialog->checkEmptytree->setChecked( false );
+	dialog->checkDebug->setChecked( false );
+	dialog->checkNoconfmem->setChecked( false );
+	dialog->checkOneshot->setChecked( false );
+	dialog->checkNoreplace->setChecked( false );
+	dialog->checkNewUse->setChecked( false );
+	
+	dialog->emergeOptionsText->clear();
+}
+
+QString EmergeInspector::getOptions()
+{
+	return dialog->emergeOptionsText->text();
+}
+
+#include "emergeinspector.moc"
---
+++ kuroo/branches/0.80.0/src/queue/results.cpp
@@ -0,0 +1,197 @@
+/***************************************************************************
+*   Copyright (C) 2004 by karye                                           *
+*   karye@users.sourceforge.net                                           *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#include "common.h"
+#include "results.h"
+#include "threadweaver.h"
+
+/**
+ * Thread for adding packages into results in db. Used by Find.
+ */
+class AddResultsPackageIdListJob : public ThreadWeaver::DependentJob
+{
+public:
+	AddResultsPackageIdListJob( QObject *dependent, const QStringList& packageIdList ) : DependentJob( dependent, "DBJob" ), m_packageIdList( packageIdList ) {}
+	
+	virtual bool doJob() {
+		DbConnection* const m_db = KurooDBSingleton::Instance()->getStaticDbConnection();
+		KurooDBSingleton::Instance()->query(" CREATE TEMP TABLE results_temp ("
+		                                    " id INTEGER PRIMARY KEY AUTOINCREMENT, "
+		                                    " idPackage INTEGER UNIQUE, "
+		                                    " flags VARCHAR(32))"
+		                                    " ;", m_db);
+		
+		KurooDBSingleton::Instance()->query( "BEGIN TRANSACTION;", m_db );
+		foreach ( m_packageIdList ) {
+			KurooDBSingleton::Instance()->insert( QString( "INSERT INTO results_temp (idPackage) VALUES ('%1');" ).arg( *it ), m_db );
+		}
+		KurooDBSingleton::Instance()->query( "COMMIT TRANSACTION;", m_db );
+		
+		// Move content from temporary table to installedPackages
+		KurooDBSingleton::Instance()->query( "DELETE FROM results;", m_db );
+		KurooDBSingleton::Instance()->insert( "INSERT INTO results SELECT * FROM results_temp;", m_db );
+		KurooDBSingleton::Instance()->query( "DROP TABLE results_temp;", m_db );
+		
+		KurooDBSingleton::Instance()->returnStaticDbConnection( m_db );
+		return true;
+	}
+	
+	virtual void completeJob() {
+		ResultsSingleton::Instance()->refresh();
+	}
+	
+private:
+	const QStringList m_packageIdList;
+};
+
+/**
+ * Thread for adding packages to results in db. Used by emerge.
+ */
+class AddResultsPackageListJob : public ThreadWeaver::DependentJob
+{
+public:
+	AddResultsPackageListJob( QObject *dependent, const EmergePackageList &packageList ) : DependentJob( dependent, "DBJob" ), m_packageList( packageList ) {}
+	
+	virtual bool doJob() {
+		DbConnection* const m_db = KurooDBSingleton::Instance()->getStaticDbConnection();
+		KurooDBSingleton::Instance()->query(" CREATE TEMP TABLE results_temp ("
+		                                    " id INTEGER PRIMARY KEY AUTOINCREMENT, "
+		                                    " idPackage INTEGER UNIQUE, "
+		                                    " flags VARCHAR(32))"
+		                                    " ;", m_db);
+		
+		KurooDBSingleton::Instance()->query( "BEGIN TRANSACTION;", m_db );
+		EmergePackageList::ConstIterator itEnd = ( m_packageList ).end();
+		for ( EmergePackageList::ConstIterator it = m_packageList.begin(); it != itEnd; ++it ) {
+			
+			// Find id for this category in db
+			QString id = KurooDBSingleton::Instance()->query( QString( "SELECT package.id FROM catSubCategory, package WHERE "
+				" catSubCategory.name = '%1'"
+				" AND package.name = '%2'"
+				" LIMIT 1;" ).arg( ( *it ).category ).arg( ( *it ).name ), m_db ).first();
+			
+			kdDebug() << "id=" << id << endl;
+			
+			if ( !id.isEmpty() )
+				KurooDBSingleton::Instance()->insert( QString( "INSERT INTO results_temp (idPackage, flags) VALUES ('%1', '%2');" ).arg( id ).arg( ( *it ).updateFlags ), m_db );
+			else
+				kdDebug() << i18n( "Can not find %1/%2-%3 in kuroo repository." ).arg( ( *it ).category ).arg( ( *it ).name ).arg( ( *it ).version ) << endl;
+		}
+		KurooDBSingleton::Instance()->query( "COMMIT TRANSACTION;", m_db );
+		
+		// Move content from temporary table to installedPackages
+		KurooDBSingleton::Instance()->query( "DELETE FROM results;", m_db );
+		KurooDBSingleton::Instance()->insert( "INSERT INTO results SELECT * FROM results_temp;", m_db );
+		KurooDBSingleton::Instance()->query( "DROP TABLE results_temp;", m_db );
+		
+		KurooDBSingleton::Instance()->returnStaticDbConnection( m_db );
+		return true;
+	}
+	
+	virtual void completeJob() {
+		ResultsSingleton::Instance()->refresh();
+	}
+	
+private:
+	const EmergePackageList m_packageList;
+};
+
+/**
+ * Object for resulting list of packages from emerge actions.
+ */
+Results::Results( QObject *parent )
+	: QObject( parent )
+{
+}
+
+Results::~Results()
+{
+}
+
+void Results::init( QObject *myParent )
+{
+	parent = myParent;
+}
+
+/**
+ * Forward signal to refresh results.
+ */
+void Results::refresh()
+{
+	emit signalResultsChanged();
+}
+
+/**
+ * Clear results.
+ * @param packageIdList
+ */
+void Results::reset()
+{
+	KurooDBSingleton::Instance()->query( "DELETE FROM results;" );
+	emit signalResultsChanged();
+}
+
+/**
+ * Launch emerge pretend of packages.
+ * @param packageList
+ */
+void Results::pretendPackageList( const QStringList& packageList )
+{
+	EmergeSingleton::Instance()->pretend( packageList );
+}
+
+/**
+ * Add packages by id to the results table in the db
+ * @param packageIdList
+ */
+void Results::addPackageIdList( const QStringList& packageIdList )
+{
+	ThreadWeaver::instance()->queueJob( new AddResultsPackageIdListJob( this, packageIdList ) );
+}
+
+/**
+ * Add packages to the results table in the db
+ * @param packageList
+ */
+void Results::addPackageList( const EmergePackageList &packageList )
+{
+	ThreadWeaver::instance()->queueJob( new AddResultsPackageListJob( this, packageList ) );
+}
+
+/**
+ * Get list of all packages.
+ * @return package list
+ */
+QStringList Results::allPackages()
+{
+	return KurooDBSingleton::Instance()->resultPackages();
+}
+
+/**
+ * Count packages in results.
+ * @return count
+ */
+QString Results::count()
+{
+	QStringList total = KurooDBSingleton::Instance()->query( "SELECT COUNT(id) FROM results LIMIT 1;" );
+	return total.first();
+}
+
+#include "results.moc"

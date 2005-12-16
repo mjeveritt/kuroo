ac614fbdc1dea43882a8900fa56d3c2bbc551dfe
karye <karye@1fa0634e-7644-4f51-99e6-489d0a0039cb>
2005-12-16 18:29:40 +0000

    Testing search functionality.

--- kuroo/branches/0.80.0/src/main.cpp
+++ kuroo/branches/0.80.0/src/main.cpp
@@ -34,7 +34,7 @@
 QRegExp pv("(-(?:\\d+\\.)*\\d+[a-z]?)");
 
 static const char description[] = I18N_NOOP("Frontend to Gentoo Portage");
-static const char version[] = "0.80.0_151205";
+static const char version[] = "0.80.0_161205";
 
 static KCmdLineOptions options[] =
 {

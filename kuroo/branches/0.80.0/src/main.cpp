923ce8fdaa31c9a8a92a778e67cc2550e7aac4e1
karye <karye@1fa0634e-7644-4f51-99e6-489d0a0039cb>
2005-12-27 18:30:14 +0000

    Added merge view.

--- kuroo/branches/0.80.0/src/main.cpp
+++ kuroo/branches/0.80.0/src/main.cpp
@@ -31,7 +31,7 @@
 #include <kdebug.h>
 
 static const char description[] = I18N_NOOP("Frontend to Gentoo Portage");
-static const char version[] = "0.80.0_261205";
+static const char version[] = "0.80.0_271205";
 
 static KCmdLineOptions options[] =
 {

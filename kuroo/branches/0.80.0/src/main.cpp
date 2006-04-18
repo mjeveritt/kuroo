75158067b30883fb32cc4b5f45d48a0471624692
karye <karye@1fa0634e-7644-4f51-99e6-489d0a0039cb>
2006-04-18 06:54:10 +0000

    Cleaning up code...

--- kuroo/branches/0.80.0/src/main.cpp
+++ kuroo/branches/0.80.0/src/main.cpp
@@ -29,7 +29,7 @@
 #include <kdebug.h>
 
 static const char description[] = I18N_NOOP("Frontend to Gentoo Portage");
-static const char version[] = "0.80.2_180406_db40";
+static const char version[] = "0.80.2_180406a_db40";
 
 static KCmdLineOptions options[] =
 {

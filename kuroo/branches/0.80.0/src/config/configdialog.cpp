1b197fe7a54a7e1778fe415f2ee3903fcc696b6a
karye <karye@1fa0634e-7644-4f51-99e6-489d0a0039cb>
2006-04-22 15:54:54 +0000

    Parsing make.conf: Skip first empty lines.

--- kuroo/branches/0.80.0/src/config/configdialog.cpp
+++ kuroo/branches/0.80.0/src/config/configdialog.cpp
@@ -641,10 +641,10 @@ bool ConfigDialog::saveMakeConf()
 		foreach ( linesConcatenated ) {
 			
 			// Skip first empty lines
-			if ( top && (*it).isEmpty() ) {
-				top = false;
+			if ( top && (*it).isEmpty() )
 				continue;
-			}
+			else
+				top = false;
 			
 			if ( (*it).contains( QRegExp( "^\\s*(CHOST|CFLAGS|CXXFLAGS|MAKEOPTS|USE|GENTOO_MIRRORS|PORTDIR_OVERLAY|FEATURES|PORTDIR|PORTAGE_TMPDIR|"
 			                              "DISTDIR|ACCEPT_KEYWORDS|AUTOCLEAN|BUILD_PREFIX|CBUILD|CCACHE_SIZE|CLEAN_DELAY|CONFIG_PROTECT|"

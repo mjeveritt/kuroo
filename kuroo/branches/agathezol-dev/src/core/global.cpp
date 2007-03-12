b67c9c5b7755edb9accb08ff676391f22ed8360f
agathezol <agathezol@1fa0634e-7644-4f51-99e6-489d0a0039cb>
2007-03-12 22:01:43 +0000

    agathezol's developement branch

---
+++ kuroo/branches/agathezol-dev/src/core/global.cpp
@@ -0,0 +1,170 @@
+/***************************************************************************
+*   Copyright (C) 2004 by karye                                           *
+*   karye@users.sourceforge.net                                           *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#include "common.h"
+#include "global.h"
+
+#include <qwidget.h>
+#include <qpalette.h>
+#include <qdatetime.h>
+
+#include <kglobal.h>
+
+/**
+ * @class Global
+ * @short Some useful global methods.
+ */
+Global::Global( QObject *parent )
+	: QObject( parent )
+{}
+
+Global::~Global()
+{}
+
+void Global::init( QObject *parent )
+{
+	m_parent = parent;
+}
+
+/**
+ * Kuroo home directory
+ */
+const QString Global::kurooDir()
+{
+	return "/var/cache/kuroo/";
+}
+
+/**
+ * Regexp to parse emerge output.
+ */
+const QRegExp Global::rxEmerge()
+{
+	if ( KurooConfig::portageVersion21() )
+		return QRegExp( "^\\[ebuild([\\s\\w]*)\\]\\s+"
+		                "((\\S+)/(\\S+))"
+		                "(?:\\s*\\[([^\\]]*)\\])?"
+		                "(?:\\s*\\[([^\\]]*)\\])?"
+		                "(?:\\s*USE=\"([^\"]*)\")?"
+		                "(?:\\s*LINGUAS=\"(?:[^\"]*)\")?"
+		                "(?:\\s(\\d*,?\\d*)\\skB)?" );
+	else
+		return QRegExp( "^\\[ebuild([\\s\\w]*)\\]\\s+"
+		                "((\\S+)/(\\S+))"
+		                "(?:\\s*\\[([^\\]]*)\\])?"
+		                "(?:\\s*\\[([^\\]]*)\\])?"
+		                "((?:\\s*[\\(\\-\\+]+\\w+[\\)%]?)*)"
+		                "(?:\\s(\\d*,?\\d*)\\skB)?" );
+}
+
+/**
+ * Parse out category, package name and version parts from package.
+ * @param sring to parse out cpv from
+ */
+const QStringList Global::parsePackage( const QString& packageString )
+{
+	QRegExp rx( "(?:[a-z]|[A-Z]|[0-9]|-)*"
+	            "("
+	            "(-(?:\\d+\\.)*\\d+[a-z]?)"
+	            "(?:_(?=alpha|beta|pre|rc|p)\\d*)?"
+	            "(?:-r\\d*)?"
+	            ")" );
+	QStringList list;
+	QString nameVersion;
+	QString package( packageString );
+	package.remove( ' ' );
+
+	// Parse out the category first
+	if ( package.contains( '/' ) ) {
+		list << package.section( "/", 0, 0 );
+		nameVersion = package.section( "/", 1, 1 );
+	}
+	else {
+		list << QString::null;
+		nameVersion = package;
+	}
+
+	// Now package name and version
+	if ( rx.search( nameVersion ) != -1 ) {
+		QString name = nameVersion.section( rx.cap( 1 ), 0, 0 );
+		list << name;
+		list << nameVersion.section( name + "-", 1 );
+		return list;
+	}
+	else
+		return QStringList::QStringList();
+}
+
+/**
+ * Kuroo widget parameters.
+ */
+void Global::setColorTheme()
+{
+	QColor c = KGlobalSettings::highlightColor();
+	m_bgColor = QString::number( c.red(), 16 ) + QString::number( c.green(), 16 ) + QString::number( c.blue(), 16 );
+	
+	c = KGlobalSettings::highlightedTextColor();
+	m_fgColor = QString::number( c.red(), 16 ) + QString::number( c.green(), 16 ) + QString::number( c.blue(), 16 );
+}
+
+/**
+ * Kuroo widget id so MessageBox's can be made modal.
+ */
+const long& Global::kurooViewId()
+{
+	return	m_wId;
+}
+
+/**
+ * Return KDE background color-theme.
+ */
+const QString& Global::bgHexColor()
+{
+	return m_bgColor;
+}
+
+/**
+ * Return KDE foreground color-theme.
+ */
+const QString& Global::fgHexColor()
+{
+	return m_fgColor;
+}
+
+/**
+ * Return duration in seconds formated as "d hh.mm.ss".
+ */
+const QString Global::formatTime( long duration )
+{
+	KLocale *loc = KGlobal::locale();
+	QString totalDays;
+	unsigned durationDays, totalSeconds;
+	
+	durationDays = duration / 86400;
+	totalSeconds = duration % 86400;
+	
+	if ( durationDays > 0 )
+		totalDays = i18n( "%1d " ).arg( QString::number( durationDays ) );
+	
+	QTime emergeTime( 0, 0, 0 );
+	emergeTime = emergeTime.addSecs( totalSeconds );
+	return totalDays + loc->formatTime( emergeTime, true, true );
+}
+
+#include "global.moc"
---
+++ kuroo/branches/agathezol-dev/src/core/images.cpp
@@ -0,0 +1,181 @@
+/***************************************************************************
+*   Copyright (C) 2004 by karye                                           *
+*   karye@users.sourceforge.net                                           *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#include "common.h"
+
+#include <qpixmap.h>
+
+#include <kiconloader.h>
+
+/**
+ * @class Images
+ * @short Delivers icons in Kuroo.
+ */
+Images::Images( QObject* m_parent )
+	: QObject( m_parent )
+{}
+
+Images::~Images()
+{}
+
+/**
+ * Load icons.
+ */
+void Images::init( QObject *parent )
+{
+	m_parent = parent;
+	
+	KIconLoader *ldr = KGlobal::iconLoader();
+	
+	pxKuroo = ldr->loadIcon( "kuroo", KIcon::NoGroup, KIcon::SizeSmallMedium, KIcon::DefaultState, NULL, true );
+	pxEmerging = ldr->loadIcon( "kuroo1", KIcon::NoGroup, KIcon::SizeSmallMedium, KIcon::DefaultState, NULL, true );
+	
+	pxCategory = ldr->loadIcon( "kuroo_category", KIcon::Small );
+	pxNew = ldr->loadIcon( "kuroo_new", KIcon::Small );
+	pxUnmerged = ldr->loadIcon( "kuroo_unmerged", KIcon::Small );
+	
+	pxPackage = ldr->loadIcon( "kuroo_package", KIcon::Small );
+	pxInstalled = ldr->loadIcon( "kuroo_stable", KIcon::Small );
+	
+	pxQueued = ldr->loadIcon( "kuroo_queue", KIcon::Small );
+	pxQueuedColumn = ldr->loadIcon( "kuroo_queue_column", KIcon::Small );
+	
+	pxWorld = ldr->loadIcon( "kuroo_world", KIcon::Small );
+	pxWorldColumn = ldr->loadIcon( "kuroo_world_column", KIcon::Small );
+	
+	pxDetails = ldr->loadIcon( "options", KIcon::Small );
+	pxRemove = ldr->loadIcon( "remove", KIcon::Small );
+	
+	pxEmpty = ldr->loadIcon( "kuroo_empty", KIcon::Small );
+	pxWarning = ldr->loadIcon( "kuroo_warning", KIcon::Small );
+
+	pxQuickPkg = ldr->loadIcon( "kuroo_quickpkg", KIcon::Small );
+	
+	pxVersionInstalled = ldr->loadIcon( "kuroo_version_installed", KIcon::Small );
+	pxInstalledColumn = ldr->loadIcon( "kuroo_installed_column", KIcon::Small );
+	
+	pxViewPackages =  ldr->loadIcon( "kuroo", KIcon::Panel );
+	pxViewQueue = ldr->loadIcon( "kuroo_queue", KIcon::Panel );
+	pxViewHistory = ldr->loadIcon( "kuroo_history", KIcon::Panel );
+	pxViewMerge = ldr->loadIcon( "kuroo_etc", KIcon::Panel );
+	pxViewLog = ldr->loadIcon( "kuroo_log", KIcon::Panel );
+}
+
+/**
+ * Deliver icons.
+ * @return pointer to pixmap
+ */
+const QPixmap& Images::icon( int image )
+{
+	switch ( image ) {
+		
+		case KUROO_READY:
+			return pxKuroo;
+			break;
+			
+		case KUROO_EMERGING:
+			return pxEmerging;
+			break;
+	
+		case EMPTY:
+			return pxEmpty;
+			break;
+		
+		case WARNING:
+			return pxWarning;
+			break;
+			
+		case INSTALLED:
+			return pxInstalled;
+			break;
+			
+		case PACKAGE:
+			return pxPackage;
+			break;
+			
+		case QUEUED:
+			return pxQueued;
+			break;
+	
+		case UNMERGED:
+			return pxUnmerged;
+			break;
+		
+		case DETAILS:
+			return pxDetails;
+			break;
+		
+		case REMOVE:
+			return pxRemove;
+			break;
+		
+		case NEW:
+			return pxNew;
+			break;
+		
+		case WORLD:
+			return pxWorld;
+			break;
+		
+		case VERSION_INSTALLED:
+			return pxVersionInstalled;
+			break;
+		
+		case QUEUED_COLUMN:
+			return pxQueuedColumn;
+			break;
+		
+		case INSTALLED_COLUMN:
+			return pxInstalledColumn;
+			break;
+		
+		case WORLD_COLUMN:
+			return pxWorldColumn;
+			break;
+		
+		case VIEW_PORTAGE:
+			return pxViewPackages;
+			break;
+		
+		case VIEW_QUEUE:
+			return pxViewQueue;
+			break;
+		
+		case VIEW_HISTORY:
+			return pxViewHistory;
+			break;
+		
+		case VIEW_MERGE:
+			return pxViewMerge;
+			break;
+		
+		case VIEW_LOG:
+			return pxViewLog;
+			break;
+		case QUICKPKG:
+			return pxQuickPkg;
+			break;
+
+		default:
+			pxEmpty;
+	}
+}
+
+#include "images.moc"

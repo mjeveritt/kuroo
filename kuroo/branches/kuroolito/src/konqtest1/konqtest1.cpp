2aa967ba8e4fcb3c2493a9fd8864ec5814ec0a24
karye <karye@1fa0634e-7644-4f51-99e6-489d0a0039cb>
2007-09-06 14:51:22 +0000

    Testing konqueror sidebar with kdevelop template..

--- kuroo/branches/kuroolito/src/konqtest1/konqtest1.cpp
+++ kuroo/branches/kuroolito/src/konqtest1/konqtest1.cpp
@@ -23,12 +23,24 @@ void konqtest1::handleURL(const KURL &url)
 	widget->setText(QString("konqtest1")+"::"+url.url());
 }
 
-
-
 extern "C"
 {
     void* create_konqsidebar_konqtest1(KInstance *instance,QObject *par,QWidget *widp,QString &desktopname,const char *name)
     {
         return new konqtest1(instance,par,widp,desktopname,name);
     }
+};
+
+extern "C" {
+        KDE_EXPORT bool add_konqsidebar_konqtest1(QString* fn, QString* param, QMap<QString,QString> *map) {
+                Q_UNUSED(param);
+                map->insert("Type", "Link");
+                map->insert("URL", "");
+                map->insert("Icon", "kuroo");
+                map->insert("Name", i18n("Portage"));
+                map->insert("Open", "true");
+                map->insert("X-KDE-KonqSidebarModule","konqsidebar_konqtest1");
+                fn->setLatin1("konqtest1%1.desktop");
+                return true;
+        }
 };

30031471535f7b6ec702004afe5491996fb21d50
karye <karye@1fa0634e-7644-4f51-99e6-489d0a0039cb>
2007-09-11 12:26:11 +0000

    Trying KCompletion on filter text field.

--- kuroo/branches/kuroolito/src/portage/portagetab.cpp
+++ kuroo/branches/kuroolito/src/portage/portagetab.cpp
@@ -51,6 +51,10 @@
 PortageTab::PortageTab( QWidget* parent )
 	: PortageBase( parent ), m_delayFilters( 0 )
 {
+	KCompletion *comp = searchFilter->completionObject();
+	 
+	// Connect to the return pressed signal - optional
+	connect( searchFilter, SIGNAL( returnPressed( const QString& ) ), comp, SLOT( addItem( const QString& ) ) );
 
 	// Connect the filters
 	connect( filterGroup, SIGNAL( released( int ) ), this, SLOT( slotFilters() ) );

3e26afff619d66af4b4ebbb1415819b1b10d20c0
galiven <galiven@1fa0634e-7644-4f51-99e6-489d0a0039cb>
2013-06-05 18:28:59 +0000

    Working on a script to automate releases.

--- kuroo4/makebranch.sh
+++ kuroo4/makebranch.sh
@@ -1,1 +1,24 @@
-svn cp https://kuroo.svn.sourceforge.net/svnroot/kuroo/kuroo4/trunk https://kuroo.svn.sourceforge.net/svnroot/kuroo/kuroo4/tags/kuroo4-0.90.2
+#!/bin/bash
+
+E_WRONGARGS=85
+if [ -n "$1" ]
+then
+	branch_name=kuroo4-$1
+else
+	echo "Usage: `makebranch.sh $0` name-of-branch"
+	exit $E_WRONGARGS
+fi
+
+svn cp https://kuroo.svn.sourceforge.net/svnroot/kuroo/kuroo4/trunk https://kuroo.svn.sourceforge.net/svnroot/kuroo/kuroo4/tags/${branch_name}
+
+cd tags
+
+svn up ${branch_name}
+
+cd ${branch_name}/src
+
+sed -e "s/static const char version\[\] = \"kuroo-9999\";/static const char version\[\] = \"${branch_name}\";/g" -i main.cpp
+
+svn ci main.cpp -m "Making a new release for ${branch_name}"
+
+
